c73be4bf2ea0391697f2772a27e012084b77dc19
Ivan Kulikov <grandeswot@gmail.com>
2021-08-01 17:39:01 +0300

    login page test extended file with MainPageLocators and LoginPageLocators added login_page.py file added

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,19 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+# def test_should_be_login_page(browser):
+#     login_page_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+#     login_page = LoginPage(browser, login_page_link)
+#     login_page.open()
+#     login_page.should_be_login_page()
